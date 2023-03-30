7a6e100fda824565dbaf34e0a6b5ebaf37e6c333
tyler2481 <sdps6503@gmail.com>
2023-03-30 14:21:33 +0800

    商品功能-實作查詢商品功能-排序

--- springboot-mail/src/main/java/com/tyler/springbootmail/dao/impl/ProductDaoImpl.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/dao/impl/ProductDaoImpl.java
@@ -26,17 +26,18 @@ public class ProductDaoImpl implements ProductDao {
     @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
         String sql ="SELECT product_id, product_name, category, image_url, price, stock, description, " +
-                "created_date, last_modified_date FROM product WHERE 1=1";
+                "created_date, last_modified_date FROM product WHERE 1=1 ";
         Map<String, Object> map = new HashMap<>();
 
         if(productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category";
+            sql = sql + " AND category = :category ";
             map.put("category", productQueryParams.getCategory().name());
         }
         if(productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name LIKE :search";
+            sql = sql + " AND product_name LIKE :search ";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
+        sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
     }
--- springboot-mail/src/main/java/com/tyler/springbootmail/dto/ProductQueryParams.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/dto/ProductQueryParams.java
@@ -6,6 +6,26 @@ public class ProductQueryParams {
     private ProductCategory category;
     private String search;
 
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    private String orderBy;
+
+    private String sort;
+
     public ProductCategory getCategory() {
         return category;
     }
