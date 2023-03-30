662ab9974792c5b8242c6ed30d9a278c79a376ef
tyler2481 <sdps6503@gmail.com>
2023-03-30 11:13:39 +0800

    商品功能-實作查詢商品列表-基礎

--- springboot-mail/src/main/java/com/tyler/springbootmail/dao/ProductDao.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.tyler.springbootmail.dao;
 import com.tyler.springbootmail.dto.ProductRequest;
 import com.tyler.springbootmail.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mail/src/main/java/com/tyler/springbootmail/service/ProductService.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/service/ProductService.java
@@ -3,7 +3,11 @@ package com.tyler.springbootmail.service;
 import com.tyler.springbootmail.dto.ProductRequest;
 import com.tyler.springbootmail.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
