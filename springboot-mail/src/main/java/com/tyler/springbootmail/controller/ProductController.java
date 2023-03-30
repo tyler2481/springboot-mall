1e565fa4d34a716b3625991eb9a0d4c2e5433ca7
tyler2481 <sdps6503@gmail.com>
2023-03-30 14:46:45 +0800

    商品功能-實作查詢商品列表-分頁

--- springboot-mail/src/main/java/com/tyler/springbootmail/controller/ProductController.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/controller/ProductController.java
@@ -6,13 +6,16 @@ import com.tyler.springbootmail.dto.ProductRequest;
 import com.tyler.springbootmail.model.Product;
 import com.tyler.springbootmail.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -25,13 +28,18 @@ public class ProductController {
            @RequestParam (required = false) String search,
            //排序
            @RequestParam (defaultValue = "created_date") String orderBy,
-           @RequestParam (defaultValue = "desc") String sort
+           @RequestParam (defaultValue = "desc") String sort,
+            //分頁
+            @RequestParam (defaultValue = "5") @Max(1000) @Min(0)Integer limit,
+            @RequestParam (defaultValue = "0") @Min(0) Integer offset
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
        List<Product> productList = productService.getProducts(productQueryParams);
        return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- springboot-mail/src/main/java/com/tyler/springbootmail/dto/ProductQueryParams.java
+++ springboot-mail/src/main/java/com/tyler/springbootmail/dto/ProductQueryParams.java
@@ -6,6 +6,26 @@ public class ProductQueryParams {
     private ProductCategory category;
     private String search;
 
+    private Integer limit;
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    private Integer offset;
+
     public String getOrderBy() {
         return orderBy;
     }
